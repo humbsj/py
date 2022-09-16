18039ea3ab5b15cf6ae0f7f1528318b62ea394d1
humbsj <humberto.sjar@gmail.com>
2022-09-16 00:50:39 -0500

    added message func in error fun

--- distance.py
+++ distance.py
@@ -10,26 +10,27 @@ def distance (x1, y1, x2, y2):
     r= math.sqrt(diffsqrt)
     print(r)
 
-def doyouneedmore(dyn):
-    if(dyn == "Y"):
-        x1i = int(input("ingrese numero x1:"))
-        y1i = int(input("ingrese numero y1:"))
-        x2i = int(input("ingrese numero x2:"))
-        y2i = int(input("ingrese numero y2:"))
-        validation_positive(x1i,y1i,x2i,y2i)
-
-        input_cn = input("Do you need more? (Y/N): ")
-        doyouneedmore(input_cn)
-
-    elif(dyn == "N"):
-        print("Thanks for using this simple programm")
-
 def message():
     x1i = int(input("ingrese numero x1:"))
     y1i = int(input("ingrese numero y1:"))
     x2i = int(input("ingrese numero x2:"))
     y2i = int(input("ingrese numero y2:"))
     validation_positive(x1i,y1i,x2i,y2i)
+
+def doyouneedmore(dyn):
+    if(dyn == "Y"):
+        #x1i = int(input("ingrese numero x1:"))
+        #y1i = int(input("ingrese numero y1:"))
+        #x2i = int(input("ingrese numero x2:"))
+        #y2i = int(input("ingrese numero y2:"))
+        #validation_positive(x1i,y1i,x2i,y2i)
+        message()
+        input_cn = input("Do you need more? (Y/N): ")
+        doyouneedmore(input_cn)
+
+    elif(dyn == "N"):
+        print("Thanks for using this simple programm")
+
 
 def errornegative():
     print("Valor debe ser mayor a 0")
