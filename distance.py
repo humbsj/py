f95405a9674196aebb2aced66091cb1aa410661e
humbsj <humberto.sjar@gmail.com>
2022-09-15 21:26:42 -0500

    validation_type idea added

--- distance.py
+++ distance.py
@@ -11,36 +11,43 @@ def distance (x1, y1, x2, y2):
     print(r)
 
 def doyouneedmore(dyn):
-        if(dyn == "Y"):
-            x1i = int(input("ingrese numero x1:"))
-            y1i = int(input("ingrese numero y1:"))
-            x2i = int(input("ingrese numero x2:"))
-            y2i = int(input("ingrese numero y2:"))
-            validation(x1i,y1i,x2i,y2i)
+    if(dyn == "Y"):
+        x1i = int(input("ingrese numero x1:"))
+        y1i = int(input("ingrese numero y1:"))
+        x2i = int(input("ingrese numero x2:"))
+        y2i = int(input("ingrese numero y2:"))
+        validation_positive(x1i,y1i,x2i,y2i)
 
-            input_cn = input("Do you need more? (Y/N): ")
-            doyouneedmore(input_cn)
+        input_cn = input("Do you need more? (Y/N): ")
+        doyouneedmore(input_cn)
 
-        elif(dyn == "N"):
-            print("Thanks for using this simple programm")
+    elif(dyn == "N"):
+        print("Thanks for using this simple programm")
 
-def error():
+def errornegative():
     print("Valor debe ser mayor a 0")
     x1i = int(input("ingrese numero x1:"))
     y1i = int(input("ingrese numero y1:"))
     x2i = int(input("ingrese numero x2:"))
     y2i = int(input("ingrese numero y2:"))
-    validation(x1i,y1i,x2i,y2i)
+    validation_positive(x1i,y1i,x2i,y2i)
     #no es necesario doyouneedmore(). 
     #Al terminar la función "validation" no existe un valor para el 
     #parámetro que pide doyouneedmore(). 
     #Es por eso que el error indica que falta un "dyn"
 
-def validation (x1, y1, x2, y2):
-        if(x1>0 and y1>0 and x2>0 and y2>0):
-            distance(x1, y1, x2, y2)
-        else:
-            error()
+def validation_positive (x1, y1, x2, y2):
+    if(x1>0 and y1>0 and x2>0 and y2>0 and 
+    type(x1) == type(1)and type(y1) == type(1)
+    and type(x2) == type(1)and type(y2) == type(1)):
+        distance(x1, y1, x2, y2)
+    else:
+        errornegative()
+
+#def validation_type (x1, y1, x2, y2):
+#    if(type(x1) == type(1)and type(y1) == type(1)
+#    and type(x2) == type(1)and type(y2) == type(1)):
+
 
 #MAIN
 
@@ -48,6 +55,6 @@ x1i = int(input("ingrese numero x1:"))
 y1i = int(input("ingrese numero y1:"))
 x2i = int(input("ingrese numero x2:"))
 y2i = int(input("ingrese numero y2:"))
-validation(x1i,y1i,x2i,y2i)
+validation_positive(x1i,y1i,x2i,y2i)
 input_c = input("Do you need more? (Y/N): ")
 doyouneedmore(input_c)
