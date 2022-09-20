46a10aca36d0d2edc1c72017121d598235ca5e58
humbsj <humberto.sjar@gmail.com>
2022-09-20 00:08:26 -0500

    str() to add more detail in the return was added

--- eureka.py
+++ eureka.py
@@ -4,13 +4,17 @@ def encuentra(cad,c): #(entrar,t)
     indice = 0
     while indice < len(cad): #6
         if cad[indice] == c:
-            return indice
+            return print("La letra"+ " "+ str(y)+ " "+ "se encuentra en la posición" + 
+            " " + str(indice+1))  
+            #aquí me indica el índice de la letra 
+            #es necesario sumar 1 al indice xq se cuenta
+            #desde el 0.
         indice = indice + 1
     return print("no hay n")
 
 #Main
 
 x = input("Coloca valor de cad: ") # entrar
-y = input("Coloca valor de c: ") # n
+y = input("Coloca valor de c: ") # t
 
 encuentra(x,y)
